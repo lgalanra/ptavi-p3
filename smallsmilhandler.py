588f9d6d28d153f5131f5692381d7130225e8b29
daviddp <david.duran.prieto@gmail.com>
2016-10-10 22:32:15 +0200

    pep8 pasado

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,6 +4,7 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
@@ -15,47 +16,45 @@ class SmallSMILHandler(ContentHandler):
             dicc['name'] = name
             dicc['width'] = attrs.get('width', '')
             dicc['height'] = attrs.get('height', '')
-            dicc['background-color'] = attrs.get('background-color','')
+            dicc['background-color'] = attrs.get('background-color', '')
             self.milista.append(dicc)
 
         elif name == 'region':
             dicc = {}
             dicc['name'] = name
-            dicc['id'] = attrs.get('id','')
-            dicc['top'] = attrs.get('top','')
-            dicc['bottom'] = attrs.get('bottom','')
-            dicc['left'] = attrs.get('left','')
-            dicc['right'] = attrs.get('right','')
+            dicc['id'] = attrs.get('id', '')
+            dicc['top'] = attrs.get('top', '')
+            dicc['bottom'] = attrs.get('bottom', '')
+            dicc['left'] = attrs.get('left', '')
+            dicc['right'] = attrs.get('right', '')
             self.milista.append(dicc)
 
         elif name == 'img':
             dicc = {}
             dicc['name'] = name
-            dicc['src'] = attrs.get('src','')
-            dicc['region'] = attrs.get('region','')
-            dicc['begin'] = attrs.get('begin','')
-            dicc['dur'] = attrs.get('dur','')
+            dicc['src'] = attrs.get('src', '')
+            dicc['region'] = attrs.get('region', '')
+            dicc['begin'] = attrs.get('begin', '')
+            dicc['dur'] = attrs.get('dur', '')
             self.milista.append(dicc)
 
         elif name == 'audio':
             dicc = {}
             dicc['name'] = name
-            dicc['src'] = attrs.get('src','')
-            dicc['begin'] = attrs.get('begin','')
-            dicc['dur'] = attrs.get('dur','')
+            dicc['src'] = attrs.get('src', '')
+            dicc['begin'] = attrs.get('begin', '')
+            dicc['dur'] = attrs.get('dur', '')
             self.milista.append(dicc)
 
         elif name == 'textstream':
             dicc = {}
             dicc['name'] = name
-            dicc['src'] = attrs.get('src','')
-            dicc['region'] = attrs.get('region','')
+            dicc['src'] = attrs.get('src', '')
+            dicc['region'] = attrs.get('region', '')
             self.milista.append(dicc)
-
 
     def get_tags(self):
         return self.milista
-
 
 if __name__ == "__main__":
     parser = make_parser()
