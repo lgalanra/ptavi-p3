6ac6f4cd1f4c4edf2480b1eba146dae0a1fea504
Luis Galan Ramil <lgalanra@gamma14.aulas.gsyc.urjc.es>
2016-10-10 10:27:16 +0200

    retocar get tags pero hecho

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -12,47 +12,57 @@ class SmallSMILHandler(ContentHandler):
     def startElement(self, name, attrs):
         if name == 'root-layout':
             dicc = {}
+            dicc['name'] = name
             dicc['width'] = attrs.get('width', '')
             dicc['height'] = attrs.get('height', '')
             dicc['background-color'] = attrs.get('background-color','')
             self.milista.append(dicc)
-            print(self.milista)
+
         elif name == 'region':
             dicc = {}
+            dicc['name'] = name
             dicc['id'] = attrs.get('id','')
             dicc['top'] = attrs.get('top','')
             dicc['bottom'] = attrs.get('bottom','')
             dicc['left'] = attrs.get('left','')
             dicc['right'] = attrs.get('right','')
             self.milista.append(dicc)
-            print(self.milista)
+
         elif name == 'img':
             dicc = {}
+            dicc['name'] = name
             dicc['src'] = attrs.get('src','')
             dicc['region'] = attrs.get('region','')
             dicc['begin'] = attrs.get('begin','')
             dicc['dur'] = attrs.get('dur','')
             self.milista.append(dicc)
-            print(self.milista)
+
         elif name == 'audio':
             dicc = {}
+            dicc['name'] = name
             dicc['src'] = attrs.get('src','')
             dicc['begin'] = attrs.get('begin','')
             dicc['dur'] = attrs.get('dur','')
             self.milista.append(dicc)
-            print(self.milista)
+
         elif name == 'textstream':
             dicc = {}
+            dicc['name'] = name
             dicc['src'] = attrs.get('src','')
             dicc['region'] = attrs.get('region','')
             self.milista.append(dicc)
-            print(self.milista)
 
-    def get_tags(lista):
-        pass
+
+    def get_tags(self):
+        for item in self.milista:
+            print(item['name'])
+            del(item['name'])
+            print(item)
+
 
 if __name__ == "__main__":
     parser = make_parser()
     myHandler = SmallSMILHandler()
     parser.setContentHandler(myHandler)
     parser.parse(open('karaoke.smil'))
+    myHandler.get_tags()
