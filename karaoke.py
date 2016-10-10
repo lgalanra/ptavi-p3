cdd6e201ccce218a8779bcc711cb63d32e8b2efe
Luis Galan Ramil <lgalanra@gamma16.aulas.gsyc.urjc.es>
2016-10-10 17:49:08 +0200

    falta pasar pep8 y json

--- karaoke.py
+++ karaoke.py
@@ -8,14 +8,19 @@ import sys
 if __name__ == "__main__":
     try:
         file = open(sys.argv[1])
-
     except ValueError:
         sys.exit('Usage: python3 karaoke.py file.smil')
-
-archivo = file.read()
 
 parser = make_parser()
 myHandler = SmallSMILHandler()
 parser.setContentHandler(myHandler)
-parser.parse(archivo)
-myHandler.get_tags()
+parser.parse(file)
+datos = myHandler.get_tags()
+
+for dicc in datos:
+    linea = ''
+    linea = linea + dicc['name']
+    for tag in dicc:
+        if tag != 'name' and dicc[tag] != '':
+            linea = linea + '\t' + tag + '="' + dicc[tag] + '"'
+    print(linea)
